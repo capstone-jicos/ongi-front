ca0317d9c7b9c58b7bd783881eba5e8f2dc1d990
Jeean Lee <ljheeeve@gmail.com>
2018-11-08 00:34:49 +0900

    initial event create page

---
+++ src/views/events/create/Confirm.vue
@@ -0,0 +1,43 @@
+<template>
+     <section class = "section">
+        <div class="container pt-lg-md">
+               <div class=" text-muted mb-2">모임 이름</div>
+                <div class=" text-muted mb-2">모임 날짜</div>
+                <div class=" text-muted mb-2">모임 시간</div>
+                <div class=" text-muted mb-2">모임 설명</div>
+                <div class=" text-muted mb-2">모임 컨셉 사진</div>
+                <div class=" text-muted mb-2">모임 참가 가능 최대 인원수 </div>
+                <div class=" text-muted mb-2">참가비</div>
+
+
+        </div>
+         <div id="attend" class="row mx-0">
+                <div class="col text-center my-auto">
+                    <router-link to="/event/create/food">
+                    <base-button type="neutral" variant="primary">이전</base-button>
+                    </router-link>
+                </div>
+                <div class="col text-center my-auto">
+                    <router-link to="/event/create/confirm">
+                    <base-button type="neutral" variant="primary">다음</base-button>
+                    </router-link>
+                </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: "confirm",
+    data(){
+        return {
+       }
+    },
+    created(){
+        this.$emit("onNavColorChange", "black")
+    }
+}
+</script>
+<style>
+</style>
+
---
+++ src/views/events/create/Food.vue
@@ -0,0 +1,64 @@
+<template>
+     <section class = "section">
+        <div class="container pt-lg-md">
+            <base-checkbox class="mb-3" v-model="checkboxes.unchecked">
+            <!-- 여기다 foods.name넣었는데 왜 안뜨지,,?뀨 -->
+                한식
+            </base-checkbox>
+            <base-checkbox class="mb-3" v-model="checkboxes.checked">
+                중식
+            </base-checkbox>      
+        </div>
+         <div id="attend" class="row mx-0">
+                <div class="col text-center my-auto">
+                    <router-link to="/event/create">
+                    <base-button type="neutral" variant="primary">이전</base-button>
+                    </router-link>
+                </div>
+                <div class="col text-center my-auto">
+                    <router-link to="/event/create/confirm">
+                    <base-button type="neutral" variant="primary">다음</base-button>
+                    </router-link>
+                </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name:"food",
+    data(){
+        return {
+            checkboxes: {
+                unchecked: false,
+                checked: true,
+                uncheckedDisabled: false,
+                checkedDisabled: true
+            },
+            foods: null
+       }
+    },
+    created(){
+        this.$emit("onNavColorChange", "black"),
+        this.foods= [
+            {
+                foodId:1,
+                name:"한식"
+            },
+            {
+                foodId:2,
+                name:"중식"
+            },
+            {
+                foodId:3,
+                name:"일식"
+            }
+        ]
+    }
+}
+</script>
+
+<style>
+
+</style>
+
