dd310a9482a444219c91f485e69d57834f430eb3
Jeean Lee <ljheeeve@gmail.com>
2018-11-13 13:44:56 +0900

    add router link

--- src/views/myPage/mypage.vue
+++ src/views/myPage/mypage.vue
@@ -6,16 +6,24 @@
         </div>
     </div>
     <div class="mt-3 py-2 border-top text-center">
+      <router-link to="/event/create">
         <h2>회원정보수정</h2>
+      </router-link>
     </div>
     <div class="mt-3 py-2 border-top text-center">
-        <h2>참가한 모임 보기</h2>
+        <router-link to="/event/create">
+          <h2>참가한 모임 보기</h2>
+        </router-link>
     </div>
     <div class="mt-3 py-2 border-top text-center">
+      <router-link to="/event/create">
         <h2>주최한 모임 보기</h2>
+      </router-link>
     </div>
     <div class="mt-3 py-2 border-top text-center">
+      <router-link to="/event/create">
         <h2>등록한 장소 보기</h2>
+      </router-link>
     </div>
     <div class="mt-3 py-2 border-top text-center">
     </div>
