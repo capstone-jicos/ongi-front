d99dcac380ea6bdd8f313282165025689ed72aed
Lawrence Kay <lkay9495@hotmail.com>
2018-11-19 15:37:08 +0900

    모임 목록 / 정보 페이지 서버 연동 확인 * [Action Required] #21 참고

--- src/views/events/Info.vue
+++ src/views/events/Info.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="profile-page">
-        <section class="section-profile-cover section-shaped my-0"
-                 v-bind:style="{ 'background-image': 'url(' + event.image + ')' }">
+        <section class="section-profile-cover section-shaped my-0">
+                 <!--v-bind:style="{ 'background-image': 'url(' + event.image + ')' }">-->
         </section>
         <section class="section section-skew event-info">
             <div class="container">
@@ -17,13 +17,13 @@
                       </div>
                         <div class='mt-3 row'>
                             <div class='col-7 float-left'>
-                                <span><i class="xi-flag-o"></i> 호스트: {{ event.host.name }}</span><br/>
-                                <span><i class="xi-home-o"></i> 장소 제공자: {{ event.provider.name }}</span>
+                                <span><i class="xi-flag-o"></i> {{ event.host.name }}</span><br/>
+                                <span><i class="xi-home-o"></i> {{ event.provider.name }}</span>
                             </div>
                             <div class='col-5'>
                                 <div class='float-right'>
-                                    <img v-lazy="event.host.profileImage" class="rounded-circle host-image mr-2">
-                                    <img v-lazy="event.provider.profileImage" class="rounded-circle host-image">
+                                    <!--<img v-lazy="event.host.profileImage" class="rounded-circle host-image mr-2">-->
+                                    <!--<img v-lazy="event.provider.profileImage" class="rounded-circle host-image">-->
                                 </div>
                             </div>
                         </div>
@@ -31,7 +31,9 @@
                             <div class="row justify-content-center">
                                 <div class="col-lg-9">
                                   <span v-html="shownDescription"></span>
-                                    <a href @click.prevent="toggleMore()">{{ showStatus }}</a>
+                                  <br/>
+                                    <a href v-if="isDescLong()"
+                                       @click.prevent="toggleMore()">{{ showStatus }}</a>
                                 </div>
                             </div>
                         </div>
@@ -70,9 +72,64 @@
 <script>
 export default {
   name: "Events",
+  data() {
+    return {
+      event: {
+        title: "",
+        description: "",
+        location: {
+          name: "",
+          country: "",
+          state: "",
+          city: "",
+          detail: "",
+          coordinates: {
+            lat: 0,
+            lng: 0
+          }
+        },
+        host: {
+          id: "",
+          name: "",
+          profileImage: ""
+        },
+        provider: {
+          id: "",
+          name: "",
+          profileImage: ""
+        },
+        feeAmount: 0,
+        type: null,
+        seats: 0,
+        date: null,
+        attendCheck: 0,
+        hostCheck: false
+      },
+      isShort: true
+    };
+  },
+  methods: {
+    toggleMore() {
+      this.isShort = !this.isShort;
+    },
+    isDescLong() {
+      return this.event.description.length > 50;
+    }
+  },
+  created() {
+    // TODO API로 모임 정보 가져와서 붙여주기
+    let eventId = this.$route.params.id;
+    let url = "/event/".concat(eventId);
+
+    this.$axios.get(url).then(res => {
+      this.event = res.data;
+    });
+
+    this.$emit("onNavColorChange", "white");
+  },
   computed: {
     shownDescription() {
-      return this.isShort
+      return this.isShort && this.isDescLong()
         ? this.event.description.substr(0, 50).concat("...")
         : this.event.description;
     },
@@ -99,58 +156,6 @@ export default {
         minute: "numeric"
       })}`;
     }
-  },
-  data() {
-    return {
-      event: {
-        title: "터키요리 해먹기 모임",
-        description:
-          "<p>터키요리 해먹으면서 노는 모임</p>" +
-          "<p>아싸 퇴근이다.</p>" +
-          "<p>에헤라디야 바람 분다.</p>" +
-          "<p>에헤라디야 바람 분다.</p>" +
-          "<p>에헤라디야 바람 분다.</p>",
-        location: {
-          name: "아주대학교",
-          address: "경기도 수원시 영통구 월드컵로 306",
-          coordinates: {
-            lat: 37.2828093,
-            lng: 127.0441714
-          }
-        },
-        // TODO: moment를 사용해서 API 측에선 Raw한 날짜 정보만 받도록
-        time: "2018년 10월 19일 오후 6시 30분",
-        fee: 50000,
-        image: "/img/theme/img-2-1200x1000.jpg",
-        host: {
-          id: "1a",
-          name: "계성혁",
-          profileImage: "/img/theme/team-4-800x800.jpg"
-        },
-        provider: {
-          id: "2a",
-          name: "꿉꿉이",
-          profileImage: "/img/theme/team-4-800x800.jpg"
-        }
-      },
-      isShort: true
-    };
-  },
-  methods: {
-    toggleMore() {
-      this.isShort = !this.isShort;
-    }
-  },
-  created() {
-    // TODO API로 모임 정보 가져와서 붙여주기
-    let eventId = this.$route.params.id;
-    let url = "/event/".concat(eventId);
-
-    this.$axios.get(url).then(res => {
-      this.event = res.data;
-    });
-
-    this.$emit("onNavColorChange", "white");
   }
 };
 </script>
