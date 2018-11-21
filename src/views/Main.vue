31268fe2f9e7ff9da1eaf8a4f55504c08343c4c6
Lawrence Kay <lkay9495@hotmail.com>
2018-11-21 15:03:39 +0900

    메인 페이지 HTML화 완성 * 사진 자체는 Transparency 를 줘서 저장함 * [Action Required] 사진 용량을 줄일 필요가 있음 * 문구는 모두 HTML화 해서 항상 중앙에 오도록 했음

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,28 @@
 <template>
   <section class="section pb-0">
     <router-link to="/event">
-      <div class="role-splitter" id="guest" ></div>
+      <div class="role-splitter" id="guest" >
+        <div class="role-description text-center">
+          <h2>Guest</h2>
+          <h6><strong>주변의 밥 모임에 참여해보세요</strong></h6>
+        </div>
+      </div>
     </router-link>
-    <router-link to="">
-      <div class="role-splitter" id="host"></div>
+    <router-link to="#">
+      <div class="role-splitter" id="host">
+        <div class="role-description text-center">
+          <h2>Host</h2>
+          <h6><strong>당신이 원하는 모임을 만들어보세요</strong></h6>
+        </div>
+      </div>
     </router-link>
-    <router-link to="">
-      <div class="role-splitter" id="provider"></div>
+    <router-link to="/my/venue">
+      <div class="role-splitter" id="provider">
+        <div class="role-description text-center">
+          <h3>Provider</h3>
+          <h6><strong>다양한 사람들을 초대해보세요</strong></h6>
+        </div>
+      </div>
     </router-link>
   </section>
 </template>
@@ -25,25 +40,50 @@ section {
   height: 100vh;
   div.role-splitter {
     height: 33.3%;
+    display: block;
+    position: relative;
+    top: 0;
+    left: 0;
     div.role-description {
-      z-index: 20;
-      height: 100%;
+      position: absolute;
+      top: 30%;
+      width: 100%;
+      color: black;
     }
   }
 
   div#guest {
-    /*background-color: #f4cc70;*/
-    background-image: url("/img/guest_font.png");
+    background: linear-gradient(
+        0deg,
+        rgba(244, 204, 112, 0.3),
+        rgba(244, 204, 112, 0.3)
+      ),
+      url(/img/guest.png);
     background-size: cover;
   }
 
   div#host {
-    background-image: url("/img/host_font.png");
+    background: linear-gradient(
+        0deg,
+        rgba(32, 148, 139, 0.3),
+        rgba(32, 148, 139, 0.3)
+      ),
+      url(/img/host.png);
     background-size: cover;
+    div.role-description {
+      * {
+        color: white;
+      }
+    }
   }
 
   div#provider {
-    background-image: url("/img/provider_font.png");
+    background: linear-gradient(
+        0deg,
+        rgba(211, 122, 34, 0.3),
+        rgba(211, 122, 34, 0.3)
+      ),
+      url("/img/provider.png");
     background-size: cover;
   }
 }
