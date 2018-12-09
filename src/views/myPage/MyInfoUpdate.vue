4254f86ab7a880635b9d42ccb7c05084c3cc2ce6
Lawrence Kay <lkay9495@hotmail.com>
2018-12-09 16:14:02 +0900

    기본 레이아웃 변경 완료 * 필요한 Method 들 정의해줘야함

--- src/views/myPage/MyInfoUpdate.vue
+++ src/views/myPage/MyInfoUpdate.vue
@@ -3,27 +3,40 @@
         <div class="container pt-lg-md">
             <form role="form" >
               <div class="mb-3">
-                   <label for="myPhoto" >프로필 사진</label>
-                    <div @click="choosePhoto()">
-                        <card class="card-profile add-my-photo" no-body>
-                        <div class="my-auto text-center">
-                            <p><i class="xi-cloud-upload-o"></i></p>
-                        </div>
-                        </card>
-                    </div>
-                    <input type="file" hidden id="myPhoto"/><br>
+                    <input type="file" hidden id="my-photo" @change="uploadPhoto"/><br>
                     <div class="container pt-lg-md text-left">
-                      <base-input label="DispayName" class="mb-2" type="text" v-model="info.displayName"></base-input>
-                      <base-input label="PW수정" class=" modifiedPw" type="password" v-model="info.pw" ></base-input>
-                      <base-input label="PW수정확인" class=" modifiedPw" type="password" v-model="info.testpw" ></base-input>
-                      <base-input label=">Gender" class="mb-2" type="text" v-model="info.gender"></base-input>
-                      <base-input label="Email" class="mb-2" type="text" v-model="info.email"></base-input>
-                      <base-input label="Country" class="mb-2" type="text" v-model="info.country"></base-input>
-                      <base-input label="State" class="mb-2" type="text" v-model="info.state"></base-input>
-                      <base-input label="City" class="mb-2" type="text" v-model="info.city"></base-input>
+                      <div class="row">
+                        <div class="col-4 text-center">
+                          <div @click="choosePhoto()">
+                            <img v-lazy="info.profileImage" class="rounded-circle host-image mx-auto">
+                          </div>
+                        </div>
+                        <div class="col-8">
+                          <base-input label="이름" class="mb-2" type="text" v-model="info.displayName"></base-input>
+                        </div>
+                      </div>
+                      <base-input label="비밀번호 수정"  type="password" v-model="info.pw" ></base-input>
+                      <base-input label="비밀번호 수정 확인" type="password" v-model="info.testpw" ></base-input>
+                      <div class="mb-3 text-center">
+                        <label>성별</label>
+                        <base-radio inline
+                                    name="m"
+                                    class="mr-5"
+                                    v-model="info.gender"
+                        >남성</base-radio>
+                        <base-radio inline
+                                    name="f"
+                                    v-model="info.gender"
+                        >여성</base-radio>
+                      </div>
+                      <hr class="my-3"/>
+                      <base-input label="이메일 주소" class="mb-2" type="text" v-model="info.email"></base-input>
+                      <base-input label="국가" class="mb-2" type="text" v-model="info.country"></base-input>
+                      <base-input label="시/도" class="mb-2" type="text" v-model="info.state"></base-input>
+                      <base-input label="시/군/구" class="mb-2" type="text" v-model="info.city"></base-input>
+                      <base-button class="float-right">저장</base-button>
                     </div>
                     <!-- 저장버튼 -->
-                    <base-button class="float-right">저장</base-button>
               </div>
             </form>
         </div>
@@ -42,7 +55,8 @@ export default {
   },
   data() {
     return {
-      info: {}
+      info: {},
+      uploadStatus: false
     };
   },
   methods: {
@@ -50,6 +64,9 @@ export default {
     ...mapActions(["setUserInfo"]),
     choosePhoto() {
       this.$el.querySelector("#my-photo").click();
+    },
+    uploadPhoto() {
+
     }
   },
   watch: {
@@ -66,11 +83,14 @@ export default {
 <style scoped lang="scss">
 @import "../../assets/scss/bootstrap/functions";
 @import "../../assets/scss/custom/variables";
-div.container {
-  div {
-    .card-profile {
-      min-height: 150px;
-    }
+img.host-image {
+  max-width: 4.5rem;
+}
+
+div {
+  label {
+    display: block;
+    text-align: left;
   }
 }
 </style>
